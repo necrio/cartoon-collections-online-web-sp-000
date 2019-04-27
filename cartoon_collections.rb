32e294245bec9c6107a57976e67fdf2095d5528b
Keith Isham <kisham8899@gmail.com>
2019-04-27 17:39:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,11 @@
 def roll_call_dwarves(dwarves)
 
 
-  i = 0
-while i < dwarves.length
-puts "#{i + 1}". "#{dwarves[i]}"
- i += 1
-end
+        i = 0
+      while i < dwarves.length
+      puts "#{i + 1}". "#{dwarves[i]}"
+       i += 1
+      end
 
 
 
