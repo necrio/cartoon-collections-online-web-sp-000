206ea11409cd4b9a8934009b2007c875a288cd75
Keith Isham <kisham8899@gmail.com>
2019-04-27 17:44:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,8 +20,23 @@ def summon_captain_planet(sum)
 
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+
+
+def long_planeteer_calls(calls_long)
+
+  i = 0
+  if calls_long.any? {|i| i.length > 4}
+    return true
+  else
+    return false
+    i += 1
+  end
+
+
+
+
+
 end
 
 def find_the_cheese# code an argument here
